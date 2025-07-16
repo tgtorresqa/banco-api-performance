f571ad52cfdd05f36df23439bbdd740d95b2ceef
tgtorresqa <158749143+tgtorresqa@users.noreply.github.com>
2025-07-16 10:13:10 -0300

    Criei um arquivo de configuração e apliquei variáveis de ambiente

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,5 +1,6 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
+import { pegarBaseURL } from '../utils/variaveis.js';
 const postLogin = JSON.parse(open('../fixtures/postLogin.json'));
 
 export const options = {
@@ -15,10 +16,9 @@ export const options = {
 };
 
 export default function () {
-    const url = 'http://localhost:3000/login';
+    const url = pegarBaseURL() + '/login';
 
     postLogin.username = "junior.lima";
-    console.log(postLogin);
     const payload = JSON.stringify(postLogin);
 
     const params = {
@@ -30,7 +30,7 @@ export default function () {
     const res = http.post(url, payload, params);
 
     check(res, {
-        'Validar que o Status é 200': (r) => r.status == 200,
+        'Validar que o Status é 200': (r) => r.status === 200,
         'Validar que o Token é string': (r) => typeof(r.json().token) == 'string'
     });
 
